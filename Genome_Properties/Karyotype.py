9a5fa461a99244ee731eac2b1a2bcc606c006ca5
Hüsna Öztoprak <45754109+HuesnaOeztoprak@users.noreply.github.com>
2026-07-07 16:19:44 +0200

    Create Karyotype.py

---
+++ Genome_Properties/Karyotype.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+fai = pd.read_csv("scaffold_lengths.txt", sep="\t", header=None, names=["chr","len"])
+with open("karyotype.txt", "w") as out:
+    for i, row in fai.iterrows():
+        out.write(f"chr - {row['chr']} {row['chr']} 0 {int(row['len'])} grey\n")
