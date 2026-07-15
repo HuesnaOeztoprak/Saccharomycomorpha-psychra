8902c9554200e01839821388c95faa61caf1e576
Hüsna Öztoprak <45754109+HuesnaOeztoprak@users.noreply.github.com>
2026-07-15 16:36:19 +0200

    Update 01_blast.sh

--- Transporter/01_blast.sh
+++ Transporter/01_blast.sh
@@ -1,15 +1,15 @@
 # Homology Search against Transporter Database
 
-#blastp \
-#  -db tcdb_blast \
-#  -query Spa255.hifiasm.v25.ragtag.pecat.braker.dedup.fasta \
-#  -out tcdb_hits.tsv \
-#  -evalue 1e-20 \
-#  -qcov_hsp_perc 50 \
-#  -max_target_seqs 5 \
-#  -outfmt 6 -num_threads 30
+blastp \
+  -db tcdb_blast \
+  -query Spa255.hifiasm.v25.ragtag.pecat.braker.dedup.fasta \
+  -out tcdb_hits.tsv \
+  -evalue 1e-20 \
+  -qcov_hsp_perc 50 \
+  -max_target_seqs 5 \
+  -outfmt 6 -num_threads 30
 
-#cut -f1 tcdb_hits.tsv | sort | uniq | wc -l
+cut -f1 tcdb_hits.tsv | sort | uniq | wc -l
         #981 
 
 awk '
