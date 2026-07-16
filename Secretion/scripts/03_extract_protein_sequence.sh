9cceaab2fc0acc9ed0f3c11a1e1abf8c5265b33f
Hüsna Öztoprak <45754109+HuesnaOeztoprak@users.noreply.github.com>
2026-07-16 15:17:01 +0200

    Update 03_extract_protein_sequence.sh

--- Secretion/scripts/03_extract_protein_sequence.sh
+++ Secretion/scripts/03_extract_protein_sequence.sh
@@ -1,4 +1,4 @@
 #only the first protID coloum
-awk '{print $1}' final_secreted_proteins.csv > final_secreted_Protein_IDs.txt
+awk '{print $1}' secreted_proteins.csv > secreted_Protein_IDs.txt
         #count 986
-wc -l final_secreted_Protein_IDs.txt
+wc -l secreted_Protein_IDs.txt
